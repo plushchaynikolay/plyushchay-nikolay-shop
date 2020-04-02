5fa69481e6a6077afc9e5c0806bf433813ee941f
Plyushchay Nikolay <n.plyushchay@napoleonit.ru>
2020-04-02 18:53:56 +0500

    - add class Product - add interface PricePrinter to print Product's price - add ConsolePricePrinter -- PricePrinter impelementation

---
+++ app/src/main/java/com/example/nikolay_plyushchay_shop/Product.kt
@@ -0,0 +1,17 @@
+package com.example.nikolay_plyushchay_shop
+
+class Product(
+    /**
+     * Must be positive
+     */
+    private val price: Double,
+    private val salePercent: Int = 0
+) {
+    /**
+     * @return price with applied discount determined by [salePercent]
+     *
+     * If [salePercent] is more than 100 than product will have negative price
+     * If [salePercent] less than 0 product price will be increased
+     */
+    fun calcDiscountPrice(): Double = price * (1 - salePercent / 100.0)
+}
