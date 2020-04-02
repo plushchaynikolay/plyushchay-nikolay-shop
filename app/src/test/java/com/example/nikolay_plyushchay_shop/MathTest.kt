ed4787486631afcf0af3fca984989ad0f8788b94
Plyushchay Nikolay <n.plyushchay@napoleonit.ru>
2020-04-02 16:12:12 +0500

    add multiplication test

--- app/src/test/java/com/example/nikolay_plyushchay_shop/MathTest.kt
+++ app/src/test/java/com/example/nikolay_plyushchay_shop/MathTest.kt
@@ -1,4 +1,11 @@
 package com.example.nikolay_plyushchay_shop
 
+import org.junit.Test
+import org.junit.Assert.*
+
 class MathTest {
+    @Test
+    fun testMultiplication() {
+        assert(2 * 3 == 6)
+    }
 }
