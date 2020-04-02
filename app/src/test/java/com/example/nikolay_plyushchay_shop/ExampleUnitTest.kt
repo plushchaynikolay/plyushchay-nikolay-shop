4eaae4d552db5ddb79bc438bb21098019431770a
Plyushchay Nikolay <n.plyushchay@napoleonit.ru>
2020-04-02 15:51:56 +0500

    Initial commit

---
+++ app/src/test/java/com/example/nikolay_plyushchay_shop/ExampleUnitTest.kt
@@ -0,0 +1,34 @@
+package com.example.nikolay_plyushchay_shop
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+
+    fun formatPrice(price: Double, measure: String = "шт.", discount: Int = 0) {
+        var result = ""
+        val priceWithDiscount = price * (1 - discount / 100)
+        if (priceWithDiscount % 1.0 > 0) {
+            result += String.format("%.2f", priceWithDiscount)
+        } else {
+            result += priceWithDiscount.toUInt()
+        }
+        result = "$result/$measure"
+        if (discount > 0) {
+            result = "$result (скидка $discount%)"
+        }
+        println(result)
+    }
+
+    @Test
+    fun addition_isCorrect() {
+        formatPrice(134.0, discount = 15)
+        println(result)
+    }
+}
