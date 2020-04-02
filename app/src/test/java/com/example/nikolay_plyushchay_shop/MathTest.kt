98da461401b9c686e788d351c067b26143acd618
Plyushchay Nikolay <n.plyushchay@napoleonit.ru>
2020-04-02 16:04:31 +0500

    add increment test

--- app/src/test/java/com/example/nikolay_plyushchay_shop/MathTest.kt
+++ app/src/test/java/com/example/nikolay_plyushchay_shop/MathTest.kt
@@ -1,4 +1,11 @@
 package com.example.nikolay_plyushchay_shop
 
+import org.junit.Test
+import org.junit.Assert.*
+
 class MathTest {
+    @Test
+    fun testIncrement() {
+        assert(1 + 2 == 3)
+    }
 }
