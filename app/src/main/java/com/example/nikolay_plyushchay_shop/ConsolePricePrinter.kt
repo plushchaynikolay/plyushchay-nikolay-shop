e42143416d94868da6065f9f1c8ec6b42dd1c47e
Plyushchay Nikolay <n.plyushchay@napoleonit.ru>
2020-04-05 00:56:43 +0500

    fix ConsolePricePrinter

--- app/src/main/java/com/example/nikolay_plyushchay_shop/ConsolePricePrinter.kt
+++ app/src/main/java/com/example/nikolay_plyushchay_shop/ConsolePricePrinter.kt
@@ -2,6 +2,7 @@ package com.example.nikolay_plyushchay_shop
 
 class ConsolePricePrinter: PricePrinter{
     override fun print(price: Double) {
-        println(price.toString())
+        val digits = if (price % 1 > 0) 2 else 0
+        println("Цена (со скидкой): %.${digits}f".format(price))
     }
 }
