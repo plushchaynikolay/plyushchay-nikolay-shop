8fa6130ee0701db2e1ae2faa0f94277f2b0bb9cb
Plyushchay Nikolay <n.plyushchay@napoleonit.ru>
2020-04-09 20:44:14 +0500

    add presenter

--- app/src/main/java/com/example/nikolay_plyushchay_shop/Bucket.kt
+++ app/src/main/java/com/example/nikolay_plyushchay_shop/Bucket.kt
@@ -1,7 +1,19 @@
 package com.example.nikolay_plyushchay_shop
 
 class Bucket(
-    val products: List<Product> = emptyList<Product>()
+    val products: List<Product> = emptyList()
 ) {
-    fun calcDiscountPrice(): Double = products.map { product -> product.calcDiscountPrice() }.sum()
+    val discountPrice: Double = products.map { product -> product.discountPrice }.sum()
+}
+
+class BucketPresenter(
+    override val view: View,
+    private val bucket: Bucket
+) : Presenter {
+    override fun print() {
+        bucket.products.forEach { product ->
+            view.print("${product.name}: ${format(product.discountPrice)}")
+        }
+        view.print(format(bucket.discountPrice))
+    }
 }
