de623aada1679385f4c29e83a0a15f25f103fc1a
Plyushchay Nikolay <n.plyushchay@napoleonit.ru>
2020-04-23 16:56:31 +0500

    product in catalog

--- app/src/main/java/com/example/nikolay_plyushchay_shop/ui/CatalogActivity.kt
+++ app/src/main/java/com/example/nikolay_plyushchay_shop/ui/CatalogActivity.kt
@@ -4,10 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.nikolay_plyushchay_shop.R
+import com.example.nikolay_plyushchay_shop.model.Product
 import kotlinx.android.synthetic.main.activity_catalog.*
 
 class CatalogActivity : BaseActivity() {
-    private val list = listOf("Пони 1", "Пони 2", "Пони 3")
+    private val list = listOf(
+        Product("Applejack", 130.0),
+        Product("Pinkie Pie", 150.0, 20),
+        Product("Rarity", 115.0)
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_catalog)
